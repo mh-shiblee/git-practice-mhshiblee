69f5ec341a775eacc51f4d6ebbcdd54df7a70d41
Mahmudul Hasan Shiblee <mahmudzim5@gmail.com>
2026-06-13 16:56:15 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+from utils import addition, subtructor
+
 name = "Mahmudul Hasan Shiblee"
 
 Date = "13th June, 2026"
 
 print(f"My name is {name}")
 print(f"Date: {Date}")
+
+summing_res = addition(29, 1)
+sub_res = subtructor(10, 5)
+print(f"Sum: {summing_res}")
+print(f"Sub: {sub_res}")
---
+++ src/utils.py
@@ -0,0 +1,10 @@
+def addition(a, b):
+    add = a + b
+
+    return add
+
+
+def subtructor(a, b):
+    sub = a - b
+
+    return sub
